485090a485f47677e6351365184485045e0c7f6b
GANESH-KORGAONKAR <ganeshkorgaonkar4335@gmail.com>
2024-12-04 16:53:25 +0530

    starting new project

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 // Add functionality for previewing images
-document.querySelectorAll(".item img").forEach(img => {
-    img.addEventListener("click", event => {
-        event.preventDefault(); // Prevent default link behavior
-        const recipeLink = img.parentElement.href; // Get recipe link
-        window.location.href = recipeLink; // Navigate to recipe page
-    });
-});
+
