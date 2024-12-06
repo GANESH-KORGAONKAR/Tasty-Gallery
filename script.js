9ae4382db64e7650b4234a41d7a724b68cfa767b
GANESH-KORGAONKAR <ganeshkorgaonkar4335@gmail.com>
2024-12-06 11:29:34 +0530

    adding recipes.html

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Add functionality for previewing images
-
+// Function to update the preview image when hovering over thumbnails
+function updatePreview(imageSrc) {
+    const previewImage = document.getElementById("preview");
+    previewImage.src = imageSrc;
+  }
+  
+  // Function to open a recipe page when clicking on a thumbnail
+  function openRecipe(recipeUrl) {
+    window.location.href = recipeUrl;
+  }
+  
